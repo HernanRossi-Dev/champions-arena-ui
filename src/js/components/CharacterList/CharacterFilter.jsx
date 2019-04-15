ee821cc0f010b1ae161032af66050a936a648f2b
HernanRossi <hernan_rossi@msn.com>
2019-04-14 21:00:13 -0700

    Remove all classes from character list

--- src/js/components/CharacterList/CharacterFilter.jsx
+++ src/js/components/CharacterList/CharacterFilter.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import { withRouter } from "react-router-dom";
 import * as cssStyles from "../../../styles/Styles.css";
@@ -14,196 +14,193 @@ import {
   Row
 } from "react-bootstrap";
 
-export class CharacterFilter extends React.Component {
+export const CharacterFilter = (props) => {
+  const [classChar, setClass] = useState('');
+  const [ancestry, setAncestry] = useState('');
+  const [level_gte, setLevelGTE] = useState('');
+  const [level_lte, setLevelLTE] = useState('');
+  const [changed, setChanged] = useState('');
 
-  static createInitFilter(oldInitFilter) {
+  const createInitFilter = (oldInitFilter) => {
     const queryString = oldInitFilter.split("&");
     const newInitFilter = {};
     const queryLength = queryString.length;
     if (queryLength > 1) {
-	    for (let i = 1; i < queryLength; i += 1) {
-		    const currentFilter = queryString[i].split("=");
-		    const key = currentFilter[0];
-		    newInitFilter[key] = currentFilter[1];
-	    }
+      for (let i = 1; i < queryLength; i += 1) {
+        const currentFilter = queryString[i].split("=");
+        const key = currentFilter[0];
+        newInitFilter[key] = currentFilter[1];
+      }
     }
     return newInitFilter;
   }
 
-  constructor(props, context) {
-    super(props);
-    CharacterFilter.createInitFilter = CharacterFilter.createInitFilter.bind(this);
-    const newInitFilter = CharacterFilter.createInitFilter(this.props.initFilter);
+  useEffect(() => {
+    const newInitFilter = createInitFilter(props.initFilter);
+    setClass(newInitFilter.class);
+    setAncestry(newInitFilter.ancestry);
+    setLevelGTE(newInitFilter.levelString);
+    setLevelLTE(newInitFilter.level_lte);
+    setChanged(false);
+  }, [props.initFilter.class, props.initFilter.ancestry, props.initFilter.level_gte, props.initFilter.level_lte]);
 
-    this.state = {
-      class: newInitFilter.class,
-      ancestry: newInitFilter.ancestry,
-      level_gte: newInitFilter.level_gte,
-      level_lte: newInitFilter.level_lte,
-      changed: false,
-    };
+  const onChangeAncestry = (e) => {
+    setAncestry(e.target.value);
+    setChanged(true);
   }
 
-  componentWillReceiveProps(newProps) {
-    this.setState({
-      class: newProps.initFilter.class,
-      ancestry: newProps.initFilter.ancestry,
-      level_gte: newProps.initFilter.level_gte,
-      level_lte: newProps.initFilter.level_lte,
-      changed: false
-    });
+  const onChangeClass = (e) => {
+    setClass(e.target.value);
+    setChanged(true);
   }
 
-  onChangeAncestry = (e) => {
-    this.setState({ ancestry: e.target.value, changed: true });
-  }
-
-  onChangeClass = (e) => {
-    this.setState({ class: e.target.value, changed: true });
-  }
-
-  onChangeLevelLte = (e) => {
+  const onChangeLevelLte = (e) => {
     const levelString = e.target.value;
     if (levelString.match(/^\d*$/)) {
-      this.setState({ level_lte: e.target.value, changed: true });
+      setLevelLTE(e.target.value);
+      setChanged(true);
+    }
+  };
+
+  const onChangeLevelGte = (e) => {
+    const levelString = e.target.value;
+    if (levelString.match(/^\d*$/)) {
+      setLevelGTE(e.target.value);
+      setChanged(true);
     }
   }
 
-  onChangeLevelGte = (e) => {
-    const levelString = e.target.value;
-    if (levelString.match(/^\d*$/)) {
-      this.setState({ level_gte: e.target.value, changed: true });
-    }
-  }
-
-  resetFilter = () => {
+  const resetFilter = () => {
     const filters = ['class', 'ancestry', 'level_gte', 'level_lte'];
-    filters.forEach((index) => {
-      if (this.props.initFilter[filters[index]] === undefined) {
-        this.props.initFilter[filters[index]] = '';
+    for ( const index of filters) {
+      if (props.initFilter[filters[index]] === undefined) {
+        props.initFilter[filters[index]] = '';
       }
-    });
-
-    this.setState({
-      class: this.props.initFilter.class,
-      ancestry: this.props.initFilter.ancestry,
-      level_gte: this.props.initFilter.level_gte,
-      level_lte: this.props.initFilter.level_lte,
-      changed: false
-    });
+    }
+    setClass(newInitFilter.class);
+    setAncestry(newInitFilter.ancestry);
+    setLevelGTE(newInitFilter.levelString);
+    setLevelLTE(newInitFilter.level_lte);
+    setChanged(false);
   }
 
-  clearFilter = () => {
-    this.props.setFilter({});
+  const clearFilter = () => {
+    props.setFilter({});
   }
 
-  applyFilter = () => {
+  const applyFilter = () => {
     const newFilter = {};
-    if (this.state.ancestry) newFilter.ancestry = this.state.ancestry;
-    if (this.state.class) newFilter.class = this.state.class;
-    if (this.state.level_gte) newFilter.level_gte = this.state.level_gte;
-    if (this.state.level_lte) newFilter.level_lte = this.state.level_lte;
-    this.props.setFilter(newFilter);
+    if (ancestry) {
+      newFilter.ancestry = ancestry;
+    }
+    if (classChar) {
+      newFilter.class = classChar;
+    }
+    if (level_gte) {
+      newFilter.level_gte = level_gte;
+    }
+    if (level_lte) {
+      newFilter.level_lte = level_lte;
+    }
+    props.setFilter(newFilter);
   }
 
-  render() {
-    return (
-      <Panel className={cssStyles.panelHeaderParent} defaultExpanded>
-        <Panel.Heading className={cssStyles.panelHeader}>
-          <Panel.Title className={cssStyles.panelHeaderText} toggle>
-            Filter Characters
+  return (
+    <Panel className={cssStyles.panelHeaderParent} defaultExpanded>
+      <Panel.Heading className={cssStyles.panelHeader}>
+        <Panel.Title className={cssStyles.panelHeaderText} toggle>
+          Filter Characters
           </Panel.Title>
-        </Panel.Heading>
-        <Panel.Collapse>
-          <Panel.Body>
-            <Row>
-              <Col xs={6} sm={4} md={3} lg={3}>
-                <FormGroup>
-                  <ControlLabel>Class</ControlLabel>
+      </Panel.Heading>
+      <Panel.Collapse>
+        <Panel.Body>
+          <Row>
+            <Col xs={6} sm={4} md={3} lg={3}>
+              <FormGroup>
+                <ControlLabel>Class</ControlLabel>
+                <FormControl
+                  componentClass="select"
+                  value={classChar}
+                  onChange={onChangeClass}
+                >
+                  <option value="">Any</option>
+                  <option value="Monk">Monk</option>
+                  <option value="Ranger">Ranger</option>
+                  <option value="Wizard">Wizard</option>
+                  <option value="Druid">Druid</option>
+                  <option value="Fighter">Fighter</option>
+                  <option value="Paladin">Paladin</option>
+                  <option value="Sorcerer">Sorcerer</option>
+                  <option value="Rogue">Rogue</option>
+                  <option value="Cleric">Cleric</option>
+                  <option value="Warlock">Warlock</option>
+                  <option value="Bard">Bard</option>
+                  <option value="Barbarian">Barbarian</option>
+                  <option value="Alchemist">Alchemist</option>
+                </FormControl>
+              </FormGroup>
+            </Col>
+            <Col xs={6} sm={4} md={3} lg={3}>
+              <FormGroup>
+                <ControlLabel>Ancestry</ControlLabel>
+                <FormControl
+                  componentClass="select"
+                  value={ancestry}
+                  onChange={onChangeAncestry}
+                >
+                  <option value="">Any</option>
+                  <option value="Human">Human</option>
+                  <option value="Dwarf">Dwarf</option>
+                  <option value="Elf">Elf</option>
+                  <option value="Gnome">Gnome</option>
+                  <option value="Half-Elf">Half-Elf</option>
+                  <option value="Half-Orc">Half-Orc</option>
+                  <option value="Halfling">Halfling</option>
+                  <option value="Goblin">Goblin</option>
+                </FormControl>
+              </FormGroup>
+            </Col>
+            <Col xs={6} sm={4} md={3} lg={3}>
+              <FormGroup>
+                <ControlLabel>Level</ControlLabel>
+                <InputGroup>
                   <FormControl
-                    componentClass="select"
-                    value={this.state.class}
-                    onChange={this.onChangeClass}
-                  >
-                    <option value="">Any</option>
-                    <option value="Monk">Monk</option>
-                    <option value="Ranger">Ranger</option>
-                    <option value="Wizard">Wizard</option>
-                    <option value="Druid">Druid</option>
-                    <option value="Fighter">Fighter</option>
-                    <option value="Paladin">Paladin</option>
-                    <option value="Sorcerer">Sorcerer</option>
-                    <option value="Rogue">Rogue</option>
-                    <option value="Cleric">Cleric</option>
-                    <option value="Warlock">Warlock</option>
-                    <option value="Bard">Bard</option>
-                    <option value="Barbarian">Barbarian</option>
-                    <option value="Alchemist">Alchemist</option>
-                  </FormControl>
-                </FormGroup>
-              </Col>
-              <Col xs={6} sm={4} md={3} lg={3}>
-                <FormGroup>
-                  <ControlLabel>Ancestry</ControlLabel>
+                    value={level_gte}
+                    onChange={onChangeLevelGte}
+                  />
+                  <InputGroup.Addon>-</InputGroup.Addon>
                   <FormControl
-                    componentClass="select"
-                    value={this.state.ancestry}
-                    onChange={this.onChangeAncestry}
+                    value={level_lte}
+                    onChange={onChangeLevelLte}
+                  />
+                </InputGroup>
+              </FormGroup>
+            </Col>
+            <Col>
+              <FormGroup>
+                <ButtonToolbar>
+                  <Button
+                    bsStyle={"primary"}
+                    onClick={applyFilter}
                   >
-                    <option value="">Any</option>
-                    <option value="Human">Human</option>
-                    <option value="Dwarf">Dwarf</option>
-                    <option value="Elf">Elf</option>
-                    <option value="Gnome">Gnome</option>
-                    <option value="Half-Elf">Half-Elf</option>
-                    <option value="Half-Orc">Half-Orc</option>
-                    <option value="Halfling">Halfling</option>
-                    <option value="Goblin">Goblin</option>
-                  </FormControl>
-                </FormGroup>
-              </Col>
-              <Col xs={6} sm={4} md={3} lg={3}>
-                <FormGroup>
-                  <ControlLabel>Level</ControlLabel>
-                  <InputGroup>
-                    <FormControl
-                      value={this.state.level_gte}
-                      onChange={this.onChangeLevelGte}
-                    />
-                    <InputGroup.Addon>-</InputGroup.Addon>
-                    <FormControl
-                      value={this.state.level_lte}
-                      onChange={this.onChangeLevelLte}
-                    />
-                  </InputGroup>
-                </FormGroup>
-              </Col>
-              <Col>
-                <FormGroup>
-                  <ButtonToolbar>
-                    <Button
-                      bsStyle={"primary"}
-                      onClick={this.applyFilter.bind(this)}
-                    >
-                      Apply
+                    Apply
                     </Button>
-                    <Button
-                      onClick={this.resetFilter}
-                      disabled={!this.state.changed}
-                    >
-                      Reset
+                  <Button
+                    onClick={resetFilter}
+                    disabled={!changed}
+                  >
+                    Reset
                     </Button>
-                    <Button onClick={this.clearFilter.bind(this)}>Clear</Button>
-                  </ButtonToolbar>
-                </FormGroup>
-              </Col>
-            </Row>
-          </Panel.Body>
-        </Panel.Collapse>
-      </Panel>
-    );
-  }
-}
+                  <Button onClick={clearFilter}>Clear</Button>
+                </ButtonToolbar>
+              </FormGroup>
+            </Col>
+          </Row>
+        </Panel.Body>
+      </Panel.Collapse>
+    </Panel>
+  );
+};
 
 CharacterFilter.propTypes = {
   setFilter: PropTypes.func.isRequired,
--- src/js/components/CharacterList/CharacterRow.jsx
+++ src/js/components/CharacterList/CharacterRow.jsx
@@ -1,96 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import { Link, withRouter } from "react-router-dom";
 import { Button, Modal } from "react-bootstrap";
 import * as cssStyles from "../../../styles/Styles.css";
 
-export class CharacterRow extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      show: false
-    };
+export const CharacterRow = (props) => {
+  const [show, setShow] = useState(false);
+
+  const acceptChanges = () => {
+    setShow(false);
+    props.deleteCharacter(props.character._id);
   }
 
-  handleClose = () => {
-    this.setState({ show: false });
-  }
-
-  acceptChanges = () => {
-    this.setState({ show: false });
-    this.deleteCharacter();
-  }
-
-  rejectChanges = () => {
-    this.setState({ show: false });
-  }
-
-  handleShow = () => {
-    this.setState({ show: true });
-  }
-
-  deleteCharacter = () => {
-    this.props.deleteCharacter(this.props.character._id);
-  }
-
-  render() {
-    const characteraID = this.props.character._id;
-    return (
-      <tr>
-        <td>{this.props.character.type}</td>
-        <td >
+  const characteraID = props.character._id;
+  return (
+    <tr>
+      <td>{props.character.type}</td>
+      <td >
+        <Link to={`/characters/${characteraID}`}>
+          {props.character.name}
+        </Link>
+      </td>
+      <td>{props.character.class}</td>
+      <td>{props.character.ancestry}</td>
+      <td>{props.character.level}</td>
+      <td>{props.character.XP}</td>
+      <td>{props.character.hitPoints}</td>
+      <td>{props.character.STR}</td>
+      <td>{props.character.DEX}</td>
+      <td>{props.character.CON}</td>
+      <td>{props.character.INT}</td>
+      <td>{props.character.WIS}</td>
+      <td>{props.character.CHA}</td>
+      <td>
+        <Button type="button" bsClass={cssStyles.deleteButton} onClick={() => setShow(true)}>
+          <i className="fas fa-times-circle fa-lg" />
+        </Button>
+      </td>
+      <td>
+        <Button type="button" bsClass={cssStyles.editButton}>
           <Link to={`/characters/${characteraID}`}>
-            {this.props.character.name}
+            <i className="far fa-edit" />
           </Link>
-        </td>
-        <td>{this.props.character.class}</td>
-        <td>{this.props.character.ancestry}</td>
-        <td>{this.props.character.level}</td>
-        <td>{this.props.character.XP}</td>
-        <td>{this.props.character.hitPoints}</td>
-        <td>{this.props.character.STR}</td>
-        <td>{this.props.character.DEX}</td>
-        <td>{this.props.character.CON}</td>
-        <td>{this.props.character.INT}</td>
-        <td>{this.props.character.WIS}</td>
-        <td>{this.props.character.CHA}</td>
-        <td>
-          <Button type="button" bsClass={cssStyles.deleteButton} onClick={this.handleShow}>
-            <i className="fas fa-times-circle fa-lg" />
-          </Button>
-        </td>
-        <td>
-          <Button type="button" bsClass={cssStyles.editButton}>
-            <Link to={`/characters/${characteraID}`}>
-              <i className="far fa-edit" />
-            </Link>
-          </Button>
-        </td>
-        <Modal
-          show={this.state.show}
-          onHide={this.handleClose}
-          className={cssStyles.createCharacterClassModal}
-        >
-          <Modal.Header closeButton>
-            <Modal.Title>Confirm Character Delete</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <div>Are you sure you want to delete this character?</div>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button onClick={this.acceptChanges}>Accept</Button>
-            <Button onClick={this.rejectChanges}>Cancel</Button>
-          </Modal.Footer>
-        </Modal>
-      </tr>
-    );
-  }
-}
+        </Button>
+      </td>
+      <Modal
+        show={show}
+        onHide={() => setShow(false)}
+        className={cssStyles.createCharacterClassModal}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Confirm Character Delete</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div>Are you sure you want to delete this character?</div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={acceptChanges}>Accept</Button>
+          <Button onClick={() => setShow(false)}>Cancel</Button>
+        </Modal.Footer>
+      </Modal>
+    </tr>
+  );
+};
 
 CharacterRow.propTypes = {
   character: PropTypes.object.isRequired,
   deleteCharacter: PropTypes.func.isRequired,
-
 };
 
 export default withRouter(CharacterRow);
